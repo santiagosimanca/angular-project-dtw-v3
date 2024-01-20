eb621751d6dbc5c7b92a3490f863c42bc5ce77e0
Darwin Andino <darandino23@gmail.com>
2024-01-20 15:03:16 -0600

    Cards Component
    He agregado el cards component, con las actualizaciones de código

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { MenuComponent } from './components/menu/menu.component';
 import { HamburguesasComponent } from './components/hamburguesas/hamburguesas.component';
 import { BurritoComponent } from './components/burrito/burrito.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { CardsComponent } from './cards/cards.component';
 
 
 @NgModule({
@@ -20,7 +21,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     MenuComponent,
     HamburguesasComponent,
     BurritoComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
