dcbcb7bf74c2bf1cc693216de7f5896ccb211717
zenon2805 <portillozenon@gmail.com>
2024-01-20 15:22:19 -0600

    Se agrega horario en componente Navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -11,4 +11,42 @@ export class NavbarComponent {
   toggleNavbar() {
     this.navbarOpen = !this.navbarOpen;
   }
+
+  // Método para ocultar la barra de navegación
+  ocultarBarra1() {
+    this.navbarOpen = false;
+  }
+
+  ocultar: boolean = true;
+  
+  ocultarBarra() {
+  this.ocultar = !this.ocultar;
+}
+
+items = [
+  {
+    "dia":"Lunes",
+    "hora":"10:00 AM - 05:00 PM"
+  },
+  {
+    "dia":"Martes",
+    "hora":"10:00 AM - 06:00 PM"
+  },
+  {
+    "dia":"Miércoles",
+    "hora":"10:00 AM - 06:00 PM"
+  },
+  {
+    "dia":"Jueves",
+    "hora":"10:00 AM - 06:00 PM"
+  },
+  {
+    "dia":"Sábado",
+    "hora":"11:00 AM - 08:00 PM"
+  },
+  {
+    "dia":"Domingo",
+    "hora":"10:00 AM - 09:00 PM"
+  }
+  ]
 }
