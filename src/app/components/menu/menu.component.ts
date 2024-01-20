95dc89f2c0d6894bb4136478b947f8ca89edb069
zenon2805 <portillozenon@gmail.com>
2024-01-20 17:04:11 -0600

    Se agrega las cards de Darwin los propertyBinding y el *ngIf

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -17,8 +17,39 @@ interface MenuItem {
 export class MenuComponent {
 
   muestraTitulo = true
-  titulo = ""
+  titulo = "Componente card"
   propertyBinding = "blue";
+
+  titulo1  = "Tasty & Bacon";
+  titulo2 = "Ranchero BBQ Burger";
+  titulo3 = "Teriyaki Twist Burger";
+  titulo4 = "Bacon Slam Burger";
+
+  Parrafo1 = true;
+  Parrafo2 = true;
+  Parrafo3 = true;
+  Parrafo4 = true;
+
+
+  sendValue1 (info:any){
+    console.log(info);
+
+
+  }
+  ocultaParrafo1(){
+    this.Parrafo1 = !this.Parrafo1;
+  }
+  ocultaParrafo2(){
+    this.Parrafo2 = !this.Parrafo2;
+  }
+  ocultaParrafo3(){
+    this.Parrafo3 = !this.Parrafo3;
+  }
+  ocultaParrafo4(){
+    this.Parrafo4 = !this.Parrafo4;
+  }
+ 
+
   userExperience: string = '';
 
   submitExperience() {
